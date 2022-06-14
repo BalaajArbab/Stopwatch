f7162c030646fca05561d4c2af2935e03ac3004d
Balaaj <95769254+BalaajArbab@users.noreply.github.com>
2022-06-14 16:59:22 -0400

    Stopwatch

---
+++ Stopwatch.h
@@ -0,0 +1,116 @@
+// A class to mimic Stopwatch functionality from System.Diagnostics in C#/.NET.
+// This is a Header only class.
+
+#ifndef STOPWATCH_H
+#define STOPWATCH_H
+
+#include <chrono>
+#include <vector>
+#include <cstddef>
+
+class Stopwatch
+{
+public:
+
+    // Initializes Stopwatch instance.
+    Stopwatch();
+
+    // Queries whether the stopwatch is currently running (.Start was called).
+    bool isRunning();
+
+    // Initiates an interval of the stopwatch
+    bool Start();
+
+    // Stops the stopwatch, creating a closed interval from the start point.
+    bool Stop();
+
+    // Returns time in seconds of currently running interval (After starting the stopwatch, without stopping it.)
+    double Elapsed();
+
+    // Returns time in seconds of all intervals + the currently running interval is stopwatch is running.
+    double TotalElapsed();
+
+    // Erases all intervals.
+    void reset();
+
+private:
+    // Type aliases to make accessing nested type easier
+    using clock_type = std::chrono::steady_clock;
+    using second_type = std::chrono::duration<double>;
+
+    std::vector<std::chrono::time_point<clock_type>> m_startPoints;
+    std::vector<std::chrono::time_point<clock_type>> m_stopPoints;
+
+    std::int32_t m_intervals{ 0 };
+    bool m_isRunning{ false };
+
+};
+
+inline Stopwatch::Stopwatch()
+{
+    m_startPoints.reserve(10);
+    m_stopPoints.reserve(10);
+}
+
+inline bool Stopwatch::isRunning() { return m_isRunning; }
+
+inline bool Stopwatch::Start()
+{
+    if (!m_isRunning)
+    {
+        m_isRunning = true;
+
+        m_startPoints.push_back(clock_type::now());
+
+        return true;
+    }
+
+    return false;
+}
+
+inline bool Stopwatch::Stop()
+{
+    if (m_isRunning)
+    {
+        m_stopPoints.push_back(clock_type::now());
+
+        m_isRunning = false;
+        ++m_intervals;
+
+        return true;
+    }
+
+    return false;
+}
+
+inline double Stopwatch::Elapsed()
+{
+    if (!m_isRunning) return 0.0;
+
+    return std::chrono::duration_cast<second_type>(clock_type::now() - m_startPoints[m_intervals]).count();
+}
+
+inline double Stopwatch::TotalElapsed()
+{
+    double total{ 0.0 };
+
+    for (int i{ }; i < m_intervals; ++i)
+    {
+        total += std::chrono::duration_cast<second_type>(m_stopPoints[i] - m_startPoints[i]).count();
+    }
+
+    total += Elapsed();
+    
+    return total;
+}
+
+inline void Stopwatch::reset()
+{
+    m_intervals = 0;
+    m_isRunning = false;
+    m_startPoints.resize(0);
+    m_stopPoints.resize(0);
+}
+
+#endif
+
